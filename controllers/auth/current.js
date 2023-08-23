ced0e2817ddfa9d89725155e8541ce71efb29a5a
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-08-23 12:06:09 +0100

    q

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,17 +1,11 @@
-const { HttpErrors } = require("../../helpers");
-const {User}=require('../../models/user')
 const current=async(req, res, next)=>{
-    const {_id:id}=req.user;
-    const user=await User.findById(id).exec()
-    if(!user){
-        throw HttpErrors(401, 'Not authorized')
-    }
+    const {email, subscription}=req.user;
     res.json({
         status:'success',
         code:200,
         user:{
-            email: user.email,
-            subscription: user.subscription
+            email,
+            subscription
           }
     })
 }
--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,17 +1,8 @@
 const { User } = require("../../models/user");
-const { HttpErrors } = require("../../helpers");
 const logout = async (req, res, next) => {
   const { _id: id } = req.user;
-  try {
-    const user = await User.findByIdAndUpdate( id, { token: null },{ new: true }).exec();
-    if (!user) {
-      throw HttpErrors(401, "Not authorized");
-    }
+    await User.findByIdAndUpdate( id, { token: null }).exec();
     res.status(204).json();
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 };
 
 module.exports = logout;
